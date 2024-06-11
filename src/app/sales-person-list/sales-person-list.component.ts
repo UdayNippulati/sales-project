3f0f7ac2212a0433e4f18267bbb169031da8531d
Uday Nippulati <nippuletiuday07@gmail.com>
2024-06-11 11:00:08 -0500

    Initial commit

---
+++ src/app/sales-person-list/sales-person-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
+
+@Component({
+  selector: 'app-sales-person-list',
+  templateUrl: './sales-person-list.component.html',
+  styleUrls: ['./sales-person-list.component.css'],
+})
+export class SalesPersonListComponent implements OnInit {
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson('uday', 'nippulati', 'nippulatiuday@gmail.com', 80),
+    new SalesPerson('dinesh', 'jalagam', 'jalagamdinesh@gmail.com', 90),
+    new SalesPerson('keerthi', 'ramagiri', 'ramagirikeerthi@gmail.com', 92),
+    new SalesPerson('vinvitha', 'akkati', 'akkativinvitha@gmail.com', 91),
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
